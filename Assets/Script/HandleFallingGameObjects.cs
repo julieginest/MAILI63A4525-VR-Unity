2946a2338a9cc316d3e847659b17c2793a831ba8
BuildTools <unconfigured@null.spigotmc.org>
2026-04-17 16:48:23 +0200

    change respawn location

--- Assets/Script/HandleFallingGameObjects.cs
+++ Assets/Script/HandleFallingGameObjects.cs
@@ -7,22 +7,22 @@ public class HandleFallingGameObjects : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-     if(transform.position.y < -10)
+        if (transform.position.y < -10)
         {
             if (isPlayer)
             {
-                transform.position =new Vector3(0, 1, 0);
+                transform.position = new Vector3(-56.9f, 5.19f, 227.9f);
             }
             else
             {
                 Destroy(this.gameObject);
             }
-        }   
+        }
     }
 }
